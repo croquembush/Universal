d095ca08a88802e3f963a47cd89917218fe05364
croquembush <akovlevnkt@gmail.com>
2023-11-24 01:34:55 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export function Header() {
+   return (
+     <div className="header">
+      <select><p>123123</p></select>
+      <input type="text" placeholder='Search' />
+      <div><img src="" alt="" /> <p>Subscribe</p></div>
+      <div className='signup'>Sign In</div>
+      
+     </div>
+   );
+}
+
+export default Header;
